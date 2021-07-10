e75c92fb16704fd5caea2fc05d0c16501a3a0d8a
Delusoire <Gh.raf@tutanota.com>
2021-07-10 11:37:47 +0000

    Improved code style, updated hard-coded features

--- JarInjector.java
+++ JarInjector.java
@@ -1,37 +1,32 @@
-import java.lang.Class;
-import java.lang.Thread;
-import java.lang.Exception;
-import java.lang.ClassLoader;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 import java.lang.reflect.Array;
-import java.lang.reflect.Method;
 import java.lang.reflect.Constructor;
-
+import java.lang.reflect.Executable;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
-
-import java.util.Map;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.jar.JarFile;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
-
-import java.io.File;
-
-import javax.swing.JOptionPane;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
-import java.lang.System;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 
 public class JarInjector extends URLClassLoader {
     public static JarInjector INSTANCE;
 
+    private JarInjector(ClassLoader parent) {
+        super(new URL[0], parent);
+    }
+
     public static int hook() {
         System.out.println("[DEBUG] Hellow from JVM!");
-        ClassLoader cl = queryUserForCL(getClassLoadersMap());
+        ClassLoader cl = queryUserForClassLoader(getClassLoadersMap());
         if (cl != null) {
             INSTANCE = new JarInjector(cl);
             return Arrays.stream(queryUserForJars())
@@ -41,50 +36,68 @@ public class JarInjector extends URLClassLoader {
         return -1;
     }
 
-    private static <V, K> Collector<V, ?, Map<K, V>> Mapify(Function<V, K> keyify) {
-        return Collectors.toMap(keyify, v -> v);
-    }
-
     private static Map<String, ClassLoader> getClassLoadersMap() {
         return Thread.getAllStackTraces().keySet().stream()
                 .map(Thread::getContextClassLoader)
                 .filter(Objects::nonNull).distinct().sorted()
-                .collect(Mapify(cl -> cl.getClass().getName()));
+                .collect(CollectionMappify(cl -> cl.getClass().getName()));
     }
 
-    private static Map<String, Method> getCallableMethodsMap(Class<?> clazz) {
-        return Arrays.stream(clazz.getDeclaredMethods())
-                .collect(Mapify(Method::toGenericString));
+    private static ClassLoader queryUserForClassLoader(Map<String, ClassLoader> cls) {
+        return cls.get(queryUserCommon("Select a ClassLoader to use for defining", cls.keySet().toArray(new String[0])));
     }
 
-    private static File[] queryUserForJars () {
+    private static File[] queryUserForJars() {
         JFileChooser jfc = new JFileChooser();
         jfc.setDialogTitle("Select jars to inject!");
         jfc.setApproveButtonText("Inject");
         jfc.setAcceptAllFileFilterUsed(false);
         jfc.addChoosableFileFilter(new FileNameExtensionFilter("JARs only", "jar"));
         jfc.setMultiSelectionEnabled(true);
-        return (jfc.showDialog(null, null) == JFileChooser.APPROVE_OPTION) ? jfc.getSelectedFiles() : new File[0];
+        return jfc.showDialog(null, null) == JFileChooser.APPROVE_OPTION
+                ? jfc.getSelectedFiles() : new File[0];
     }
 
-    private static String queryUserCommon(Object message, String[] options) {
-        return (String) JOptionPane.showInputDialog(null, message, "JarInjector - by Delusional", JOptionPane.INFORMATION_MESSAGE, null, options, null);
-    }
+    private static boolean loadJar(File jar) {
+        Class<?> Main;
+        try {
+            INSTANCE.addURL(jar.toURI().toURL());
+            Main = INSTANCE.loadClass(queryUserForMainClass(jar), true);
+        } catch (Exception ignored) {
+            return false;
+        }
 
-    private static ClassLoader queryUserForCL(Map<String, ClassLoader> cls) {
-        return cls.get(queryUserCommon("Choose The ClassLoader (cancel if none matches)", cls.keySet().toArray(new String[0])));
+        Map<String, Constructor<?>> cstrs = getCstrMapOfClass(Main);
+        Map<String, Method> methods = getMethodMapOfClass(Main);
+        String[] execs = Stream.concat(cstrs.keySet().stream(), methods.keySet().stream()).toArray(String[]::new);
+
+        while (true) {
+            String exec = queryUserCommon("Select an Executable to be evaluated", execs);
+            if (Objects.isNull(exec)) break;
+
+            try {
+                Executable ex;
+                if (Objects.nonNull(ex = cstrs.get(exec))) ((Constructor<?>) ex).newInstance(parseArgsForExec(ex));
+                if (Objects.nonNull(ex = methods.get(exec))) ((Method) ex).invoke(parseArgsForExec(ex));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return true;
     }
 
     private static String queryUserForMainClass(File jar) throws Exception {
-        return queryUserCommon("Choose The Main class", new JarFile(jar).stream()
+        return queryUserCommon("Select a Class to be treated as Main", new JarFile(jar).stream()
                 .map(ZipEntry::getName)
                 .filter(name -> !name.startsWith("META-INF/") && name.endsWith(".class"))
                 .map(name -> name.substring(0, name.length() - 6).replace("/", "."))
                 .sorted().toArray(String[]::new));
     }
 
-    private static Method queryUserForCallee(Map<String, Method> methods) {
-        return methods.get(queryUserCommon("Choose the method to call", methods.keySet().toArray(new String[0])));
+    private static String queryUserCommon(Object message, String[] options) {
+        return (String) JOptionPane.showInputDialog(null, message, "JarInjector - by Delusional",
+                JOptionPane.INFORMATION_MESSAGE, null, options, null);
     }
 
     // Living example of why java error handling suxsce
@@ -92,7 +105,7 @@ public class JarInjector extends URLClassLoader {
         Object ret = null;
         Constructor<?> cstr;
         if (clazz.isArray()) {
-          ret = Array.newInstance(clazz.getComponentType(), 0);
+            ret = Array.newInstance(clazz.getComponentType(), 0);
         } else {
             try {
                 cstr = clazz.getDeclaredConstructor();
@@ -107,31 +120,37 @@ public class JarInjector extends URLClassLoader {
             }
             try {
                 ret = cstr.newInstance();
-            } catch (Exception e) { e.printStackTrace(); }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return ret;
     }
 
-    private static boolean loadJar(File jar) {
-        boolean ret = false;
-        try {
-            INSTANCE.addURL(jar.toURI().toURL());
-            Class<?> Main = INSTANCE.loadClass(queryUserForMainClass(jar), true);
-            ret = true;
 
-            safeInstantiate(Main);
-            Map<String, Method> methods = getCallableMethodsMap(Main); Method method;
-            while ((method = queryUserForCallee(methods)) != null)
-                try {
-                    method.invoke(Main, (Object[]) Arrays.stream(method.getParameterTypes())
-                            .map(JarInjector::safeInstantiate)
-                            .toArray(Object[]::new));
-                } catch (Exception e) { e.printStackTrace(); }
-        } catch (Exception ignored) { }
-        return ret;
+    private static <V, K> Collector<V, ?, Map<K, V>> CollectionMappify(Function<V, K> keyify) {
+        return Collectors.toMap(keyify, v -> v);
     }
 
-    private JarInjector(ClassLoader parent) {
-        super(new URL[0], parent);
+    private static <T> Stream<T> arraysToStream(T[] a, T[] b) {
+        return Stream.concat(Arrays.stream(a), Arrays.stream(b));
+    }
+
+    private static <K, V> Map<K, V> arraysToMap(V[] a, V[] b, Function<V, K> keyify) {
+        return arraysToStream(a, b).distinct().collect(CollectionMappify(keyify));
+    }
+
+    private static Map<String, Constructor<?>> getCstrMapOfClass(Class<?> clazz) {
+        return arraysToMap(clazz.getDeclaredConstructors(), clazz.getConstructors(), Constructor::toGenericString);
+    }
+
+    private static Map<String, Method> getMethodMapOfClass(Class<?> clazz) {
+        return arraysToMap(clazz.getDeclaredMethods(), clazz.getMethods(), Method::toGenericString);
+    }
+
+    private static Object[] parseArgsForExec(Executable ex) {
+        return Arrays.stream(ex.getParameterTypes())
+                .map(JarInjector::safeInstantiate)
+                .toArray(Object[]::new);
     }
 }
